9469a5533e97391143cde157e8acb450e8b18df2
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-22 17:58:08 -0700

    Further!

--- hackbright.py
+++ hackbright.py
@@ -90,8 +90,38 @@ def assign_grade(github, title, grade):
     db.session.execute(QUERY, {'git': github, 
                                'new_title': title,
                                'new_grade': grade})
-    db.session .commit()
+    db.session.commit()
     print(f"Successfully added grade {grade} to {github}'s {title}")
+
+
+def add_project(title, descript, max_grade):
+    QUERY = """INSERT INTO projects (title, description, max_grade)
+        VALUES (:new_title, :description, :grade)"""
+
+    db.session.execute(QUERY, {'new_title': title, 
+                               'description': descript,
+                               'grade': max_grade})
+    db.session.commit()
+    print(f"Successfully added project {title} with grade {max_grade}")
+
+
+def get_all_grades_from_student(first_name, last_name):
+    """Given first and last name, return all grades and project title."""
+
+    QUERY = """
+        SELECT project_title, grade
+        FROM students
+            JOIN grades ON (github=student_github)
+        WHERE first_name = :f_name AND last_name = :l_name
+    """
+
+    db_cursor = db.session.execute(QUERY, {'f_name': first_name,
+                               'l_name': last_name})
+
+    row = db_cursor.fetchall()
+    print(f"{first_name} {last_name} has the following projects and grades:")
+    for title, grade in row:
+        print(f"{title} {grade}")
 
 
 def handle_input():
@@ -104,33 +134,47 @@ def handle_input():
     command = None
 
     while command != "quit":
-        input_string = input("HBA Database> ")
-        tokens = input_string.split()
-        command = tokens[0]
-        args = tokens[1:]
+        try:
+            input_string = input("HBA Database> ")
+            tokens = input_string.split()
+            command = tokens[0]
+            if command == 'add':
+                if len(tokens[1:] < 3):
+                    raise ValueError
+                title = tokens[1]
+                max_grade = tokens[-1]
+                description = " ".join(tokens[2:-1])
+                add_project(title, description, max_grade)
+            else:
+                args = tokens[1:]
 
-        if command == "student":
-            github = args[0]
-            get_student_by_github(github)
+                if command == "student":
+                    github = args[0]
+                    get_student_by_github(github)
 
-        elif command == "new_student":
-            first_name, last_name, github = args  # unpack!
-            make_new_student(first_name, last_name, github)
+                elif command == "new_student":
+                    first_name, last_name, github = args  # unpack!
+                    make_new_student(first_name, last_name, github)
 
-        elif command == "title":
-            title = args[0]
-            get_project_by_title(title)
+                elif command == "title":
+                    title = args[0]
+                    get_project_by_title(title)
 
-        elif command == "grade":
-            get_grade_by_github_title(args[0], args[1])
+                elif command == "grade":
+                    get_grade_by_github_title(args[0], args[1])
 
-        elif command == "assign":
-            github, title, grade = args
-            assign_grade(github, title, grade)
+                elif command == "assign":
+                    github, title, grade = args
+                    assign_grade(github, title, grade)
 
-        else:
-            if command != "quit":
-                print("Invalid Entry. Try again.")
+                elif command == "all":
+                    get_all_grades_from_student(args[0], args[1])
+
+                else:
+                    if command != "quit":
+                        print("Invalid Entry. Try again.")
+        except ValueError:
+            print('ERROR!')
 
 
 if __name__ == "__main__":
